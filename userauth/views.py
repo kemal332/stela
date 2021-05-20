c5f9cfedec3bad81809baa8519af00cf5bd46cb7
unknown <luka.macan2@fer.hr>
2021-05-20 18:50:07 +0200

    registration with specific attributes

--- userauth/views.py
+++ userauth/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import UserForm
+from .forms import ProfileForm
+from django.db import transaction
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-def register(response):
-    if response.user.is_authenticated:
-        return redirect("/")
-
-    if response.method == "POST":
-        #   creating new user
-        form = RegisterForm(response.POST)
-        if form.is_valid():
-            form.save()
-        return redirect("/")
+@transaction.atomic
+def register(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.refresh_from_db()
+            profile_form = ProfileForm(request.POST, instance=user.profile)  # Reload the profile form with the profile instance
+            profile_form.full_clean()
+            profile_form.save() 
+            messages.success(request, ('Your profile was successfully created!'))
+            return redirect('/')
     else:
-        form = RegisterForm()
-
-    return render(response, "register/register.html", {"form": form})
+        user_form = UserForm()
+        profile_form = ProfileForm()
+    return render(request, "register/register.html", {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
